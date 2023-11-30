71539a5bf485319d0c9cdc1598906fa0e8059f5e
Amalia Minzat <minzatamalia010@gmail.com>
2023-11-30 16:43:14 +0200

    htaccess and notes file added

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 require 'functions.php';
-require 'Database.php'; //Database needs to load before router
+require 'Database.php'; //Database needs to load before router so we can see the class database
 require 'router.php';
-
 

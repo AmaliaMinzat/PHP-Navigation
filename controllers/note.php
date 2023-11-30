17569761e78ff98de89a3809dfce23627004edbe
Amalia Minzat <minzatamalia010@gmail.com>
2023-11-30 18:04:13 +0200

    Authorization method added for notes and user id

--- controllers/note.php
+++ controllers/note.php
@@ -4,9 +4,23 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'Note';
+$currentUserId = 1;
 
-$note = $db->query('select * from notes where id = :id',['id' => $_GET['id']])->fetch();
+$note = $db->query(
+    'select * from notes where id = :id', /* track the note that has am id 
+that matches the one in the query string : http://navigation.test/note?id=2*/
+    [
+        'id' => $_GET['id']
+    ]
+)->fetch();
+
+if (!$note) { //when asking for a non existent id => 404 not found
+    abort();
+}
 
 
+if ($note['user_id'] !== $currentUserId) { //if the notes where not made from current user => 403 forbidden
+    abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 
 require 'functions.php';
 require 'Database.php'; //Database needs to load before router so we can see the class database
+require 'Response';
 require 'router.php';
 

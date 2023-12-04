cbf14e2e1b15e156ee8b4a822eea97e7e68448ee
Amalia Minzat <minzatamalia010@gmail.com>
2023-12-04 16:47:08 +0200

    Validation form added

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,15 +1,33 @@
 <?php
 
+require 'Validator.php';
+
 $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'Create Note';
 
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+
+    $errors = [];
+
+    //$validator = new Validator(); if the function is static this line can be removed and called inline bellow
+
+    if (! Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'A body of no more than 1,000 characters is required.';
+    }
+     // :: call a static method on a class, works just for static functions
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
+
+
 }
 
 require 'views/note-create.view.php';
